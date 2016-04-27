99f142d7db57da862b74386bd3a2bcd6933383bd
Jared Billig <jared.billig@gmail.com>
2016-04-27 16:54:58 -0400

    wrote getAllPuppies function

--- queries.js
+++ queries.js
@@ -1,5 +1,11 @@
+/*
+Create an instance of pg-promise, assigned to pgp
+*/
+
 var promise = require('bluebird');
 
+// Overwrite pg-promise's default promise library - ES6 Promises -
+// with Bluebird
 var options = {
   promiseLib: promise
 }
@@ -8,6 +14,21 @@ var connectionString = 'postgres://localhost:5432/puppies';
 var db = pgp(connectionString);
 
 // add query functions
+
+function getAllPuppies(req, res, next) {
+  db.any('select * from pups')
+    .then(function(data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
 
 module.exports = {
   getAllPuppies  : getAllPuppies,
